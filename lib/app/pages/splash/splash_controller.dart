f68fa39c77dd06d6caf6fca0eea216955b1ff7ba
Umut <umutcoskun2017@gmail.com>
2022-01-27 19:33:08 +0300

    removed splash

--- lib/app/pages/splash/splash_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
-
-class SplashController extends Controller {
-  @override
-  void initListeners() {
-    // TODO: implement initListeners
-  }
-}
--- lib/app/pages/splash/splash_presenter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
-
-class SplashPresenter extends Presenter {
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
-}
